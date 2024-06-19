bc88ba13d382dcc08bd8f71cb4ce4263286ea9ff
Mati4321 <161222178+Mati4321@users.noreply.github.com>
2024-06-19 18:40:39 -0300

    Agrego las clases Docente y Profesor
    La clase Profesor debe ser hija de la clase Docente

---
+++ Instituto/Docente.cs
@@ -0,0 +1,60 @@
+﻿namespace Instituto
+{
+    class Docente
+    {
+        private ulong Legajo;    // UNICO
+        private ulong AñosServicio;
+        private float Sueldo;
+        private string Nombres;
+        private string Apellidos;
+        private string Formacion;
+
+        // Constructor
+        public Docente(ulong leg, ulong añosservicio, float suel, string nom, string ape, string formacion)
+        {
+            Legajo = leg;
+            AñosServicio = añosservicio;
+            Sueldo = suel;
+            Nombres = nom;
+            Apellidos = ape;
+            Formacion = formacion;
+        }
+
+        // Setters
+        public void SetLegajo(ulong leg)
+        { Legajo = leg; }
+
+        public void SetAñosServicio(ulong añosservicio)
+        { AñosServicio = añosservicio; }
+
+        public void SetSueldo(float sueldo)
+        { Sueldo = sueldo; }
+
+        public void SetNombres(string nom)
+        { Nombres = nom; }
+
+        public void SetApellidos(string ape)
+        { Apellidos = ape; }
+
+        public void SetFormacion(string formacion)
+        { Formacion = formacion; }
+
+        // Getters
+        public ulong GetLegajo() 
+        { return Legajo; }
+
+        public ulong GetAñosServicio()
+        { return AñosServicio; }
+
+        public float GetSueldo()
+        { return Sueldo; }
+        public string GetNombres() 
+        { return Nombres; }
+
+        public string GetApellidos() 
+        { return Apellidos; }
+
+        public string GetFormacion()
+        { return Formacion; }
+    }
+}
