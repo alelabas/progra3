2d46fedd4e140eee94252a3efc480bd16b4215bf
alelabas <alelabas208@gmail.com>
2024-06-20 12:42:08 -0300

    20/6 12.42

--- ListaDivisiones.cs
+++ ListaDivisiones.cs
@@ -6,5 +6,17 @@ namespace Instituto
 
     public class ListaDivisiones
     {
+        private ArrayList listaDivisiones;
+
+        public ListaDivisiones()
+        {
+            this.listaDivisiones = new ArrayList();
+        }
+
+        public AgregarDivision(UInt32 anio, char letra, UInt32 aula, string? tutor)
+        {
+            listaDivisiones.add(new Divisiones(anio, letra, aula, tutor));
+        }
+
     }
 }
